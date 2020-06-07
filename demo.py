a41d18514441f131a686656376e563920c845030
kimhai <kimhaiareal@gmail.com>
2020-06-07 12:25:36 +0700

    refactor code base

--- demo.py
+++ demo.py
@@ -3,4 +3,5 @@ import pandas as pd
 df = pd.DataFrame({'name': ['Hai', 'Vy']})
 df1 = pd.DataFrame({'age': [26,25]})
 df2 = pd.DataFrame({'city': ['HCM', 'QN']})
+df3 = pd.DataFrame({'location':['112', '13']})
 print(df1)
--- demo2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
- 
-df4 = pd.DataFrame({'tall': [180,155]})
-
-print(df4)
